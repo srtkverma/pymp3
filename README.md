507a0d8181983789139e9d92cf212f0744916eae
Sarthak <srtkverma@gmail.com>
2019-07-22 18:22:32 +0530

    first version that really works and add readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Pymp3
+A personal fun python project
+
+REQUIREMENTS:
+- Python3
+- BeautifulSoup
+- Doesn't work on windows(uses `wget` for downloading)
+
+USAGE:
+1. Download the script and run via `python3 main.py`
+2. Enter name of song
+3. Enter `yes` if the name matches, `no` if it doesn't match
+4. If you enter `yes`, it will download the file.
+5. If you enter `no`, it will ask again for the name, add artist/album/movie for better results
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup as bs
 import requests
-import os
 
-def download(url,song_name):
-    os.system("wget -q --show-progress -O '{}' '{}'".format(song_name,url))
+def download(url,vid):
+    header = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"}
+    result = requests.get(url, stream=True, headers=header)
+    return result
 
 def find_all(string,to_find="/"):
     pos=[]
@@ -28,9 +29,24 @@ while True:
         continue
     slashes = find_all(song_url)
     vid=song_url[slashes[1]+1:len(song_url)]
-    downloadpage_url = "https://mp3download.center/download/{}".format(vid)
-    downloadpage_response = get_response(downloadpage_url)
+    downloadpage_response = get_response("https://mp3download.center/download/{}".format(vid))
     quality = downloadpage_response.find("button",{"class":"download-mp3-url"})["id"][3:6]
     final_url = "https://mp3download.center/get-file?vid={}&quality={}&title={}".format(vid,quality,title)
-    download(final_url,f"{title}.mp3")
+    print("Creating file for download....")
+    requests.post("https://mp3download.center/trigger-download",data={"vid":vid, "quality":"320"})
+    while True:
+        response = download(final_url,vid)
+        if response.headers['content-type']=='audio/mpeg':
+            break
+        else:
+            continue
+    file_name = title
+    handle = open(file_name, "wb")
+    total_bytes=0
+    for chunk in response.iter_content(chunk_size=512):
+        total_bytes+=512
+        if chunk:
+            handle.write(chunk)
+        print(f"{round((total_bytes/1000)/1000,1)}MB", end='\r')
+    print(f"Downloaded Complete({((total_bytes)/1000)/1000} MB)")
     break
